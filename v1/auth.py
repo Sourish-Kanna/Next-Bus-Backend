b23403ef2f34a5c4a018decd6db1c6a54e2fbf22
Sourish Kanna <sourishkanna001@gmail.com>
2025-05-21 18:12:31 +0530

    Add logging decorator for authentication endpoints

--- v1/auth.py
+++ v1/auth.py
@@ -1,13 +1,16 @@
 from fastapi import APIRouter
+from .logging_cust import log_activity
 
 auth_router = APIRouter(prefix="/auth", tags=["Authentication"])
 
 @auth_router.get("/login")
+@log_activity
 async def login() -> dict[str, str]:
     """Simulate a login endpoint."""
     return {"message": "Login successful"}
 
 @auth_router.post("/register")
-async def register() -> dict[str, str]:
+@log_activity
+def register() -> dict[str, str]:
     """Simulate a registration endpoint."""
     return {"message": "Registration successful"}
---
+++ v1/logging_cust.py
@@ -0,0 +1,22 @@
+import functools
+import asyncio
+
+def log_activity(func):
+    @functools.wraps(func)
+    def sync_wrapper(*args, **kwargs):
+        print("Decorator: sync Function is being called")
+        x = func(*args, **kwargs)
+        print(f"Decorator: Function returned {x}")
+        return x
+
+    @functools.wraps(func)
+    async def async_wrapper(*args, **kwargs):
+        print("Decorator: async Function is being called")
+        x = await func(*args, **kwargs)
+        print(f"Decorator: Function returned {x}")
+        return x
+
+    if asyncio.iscoroutinefunction(func):
+        return async_wrapper
+    else:
+        return sync_wrapper
